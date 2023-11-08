779423fce049d369a08b36833b341cc475846b2f
skd28 <shivajaldwivedi@gmail.com>
2023-11-08 14:35:07 +0530

    update for reponsive

--- app/components/First.js
+++ app/components/First.js
@@ -85,81 +85,60 @@ function First() {
 
     return (
         <>
-            <div className=' bg-blue-50 h-screen'
+            <div className=" bg-blue-50 h-5/6  bg-cover bg-center bg-[url('/mobile.jpg')] md:bg-[url('/Desktop.jpg')]"
                 style={{
-                    backgroundImage: `url('/Desktop.jpg')`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
+                    // backgroundImage: `url('/Desktop.jpg')`, lg:bg-[url('/Desktop.jpg')]  md:bg-[url('/mobile.jpg')]
                 }}
             >
-                <div className='px-20 pt-10'>
+                <div className='px-5 pt-10 xs:px-10 md:px-7 lg:px-16 '>
                     <img src='/logo.png' alt='logo' className='  w-20 h-7 ' />
-
                 </div>
-                <div className=' w-full h-4/5 px-20 pt-8' style={{
-                    //border: '2px solid red',
-                }}>
-                    <div style={{
-                        float: 'left',
-                        width: '50%',
-                    }}>
-                        <h1 className='text-white text-[60px] font-semibold' style={{
-                            //border: '2px solid red'
-                        }}>Get ready for</h1>
-                        <h1 className='text-white text-[60px] font-semibold bottom-4 relative' style={{
-                            //border:  '2px solid blue'
-                        }}>your</h1>
-                        <h1 className='text-[#61ABEB] text-[60px] font-semibold bottom-8 relative'> technological</h1>
-                        <h1 className='text-[#61ABEB] text-[60px] font-semibold bottom-16 relative'>journey.</h1>
+                <div className=' w-full h-4/5  pt-8 grid grid-flow-row md:grid-flow-col ' >
+                    <div className=' mx-auto md:mx-0 md:px-7  lg:px-16' >
+                        <h1 className='text-white text-[2rem] font-semibold xs:text-[4rem] sm:text-[5rem] md:text-[4rem] lg:text-[5rem]' >Get ready for</h1>
+                        <h1 className='text-white text-[2rem] font-semibold bottom-4 relative xs:text-[4rem] sm:text-[5rem] md:text-[4rem] lg:text-[5rem]' >your</h1>
+                        <h1 className='text-[#61ABEB] text-[2rem] font-semibold bottom-5 relative xs:text-[4rem] sm:text-[5rem] md:text-[4rem] lg:text-[5rem]'> technological</h1>
+                        <h1 className='text-[#61ABEB] text-[2rem] font-semibold bottom-5 relative xs:text-[4rem] sm:text-[5rem] md:text-[4rem] lg:text-[5rem]'>journey.</h1>
                         <p className='text-white bottom-5 relative '>We are halfway....</p>
                     </div>
 
-                    <div className='h-full text-center items-center justify-center bottom-8 relative ' style={{
-                        //border: '2px solid blue',
-                        float: 'right',
-                        width: '50%',
-                    }}>
-                        <div className='' style={{
-                            float: 'left',
-                            width: '50%',
-                        }}>
-                            <span className='text-[90px] font-semibold text-white' style={{
-                                //  border: '2px solid yellow'
-                            }} >
-                                {countdown.days}
-                            </span>
-                            <p className='text-white text-[30px] w-1/3 mx-16 bottom-4 relative' style={{
-                                //border: '2px solid green'
-                            }}>Days</p>
-                            <span className='text-[90px] font-semibold text-white' >
-                                {countdown.minutes}
-                            </span>
-                            <p className='text-white text-[30px] w-1/3 mx-16 bottom-4 relative' style={{
-                                //border: '2px solid green'
-                            }}>Minutes</p>
-
+                    <div className='text-center items-center justify-center relative grid grid-flow-row mx-auto  md:mx-0' >
+                        <div className='grid grid-flow-col gap-20  md:gap-10 '>
+                            <div>
+                                <span className='text-[4rem] font-semibold text-white xs:text-[5rem] sm:text-[6rem] md:text-[5rem] lg:text-[6rem]'  >
+                                    {countdown.days}
+                                </span>
+                                <p className='text-white text-[1rem] w-3/5  bottom-4 relative  mx-auto ' >Days</p>
+                            </div>
+                            <div >
+                                <span className='text-[4rem] font-semibold text-white  xs:text-[5rem] sm:text-[6rem] md:text-[5rem] lg:text-[6rem]' >
+                                    {countdown.hours}
+                                </span>
+                                <p className='text-white text-[1rem] w-3/5  bottom-4 relative  mx-auto '>Hours</p>
+                            </div>
                         </div>
-                        <div className='' style={{
-                            float: 'right',
-                            width: '50%',
-                        }}>
-                            <span className='text-[90px] font-semibold text-white' >
-                                {countdown.hours}
-                            </span>
-                            <p className='text-white text-[30px] w-1/3 mx-16 bottom-4 relative'>Hours</p>
-                            <span className='text-[90px] font-semibold text-white' >
-                                {countdown.seconds}
-                            </span>
-                            <p className='text-white text-[30px] w-1/3 mx-16 bottom-4 relative'>Seconds</p>
+                        <div className='grid grid-flow-col gap-20 md:gap-10 '>
+                            <div>
+                                <span className='text-[3rem] font-semibold text-white  xs:text-[5rem] sm:text-[6rem] md:text-[5rem] lg:text-[6rem]' >
+                                    {countdown.minutes}
+                                </span>
+                                <p className='text-white text-[1rem] w-3/4  bottom-4 relative mx-auto ' >Minutes</p>
+                            </div>
+                            <div>
+                                <span className='text-[4rem] font-semibold text-white  xs:text-[5rem] sm:text-[6rem] md:text-[5rem] lg:text-[6rem]' >
+                                    {countdown.seconds}
+                                </span>
+                                <p className='text-white text-[1rem] w-3/4 bottom-4 relative  mx-auto  '>Seconds</p>
+                            </div>
 
                         </div>
 
                     </div>
                 </div>
-                <div className=''>
+                <div className=' pt-16'>
                     <p className='text-white text-center text-xs '>Copyrights @ Root Integrated Technologies Pvt. Ltd 2023</p>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
