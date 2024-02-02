ff4d5df0a4a4c8394490702a001a11c006676670
skd28 <shivajaldwivedi@gamil.com>
2024-02-02 10:24:40 +0530

    update on screen sizes display

--- app/components/New_Page.js
+++ app/components/New_Page.js
@@ -11,7 +11,7 @@ function New_Page() {
   return (
     <>
       <div
-        className=" lg:bg-blue-800 overflow-hidden bg-cover lg:h-screen "
+        className=" bg-blue-800 overflow-hidden bg-cover 2xl:h-screen "
         style={{
           backgroundImage: "url(/new_background.jpg)",
         }}
